59d88f21e645cdb44df3f296e778aace334e37e3
krishna kumar <krishnakumar356808@gmail.com>
2022-01-16 16:02:00 +0530

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { lorem, random } from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing';
+  randomText = lorem.sentences();
+  solved = false;
+  typedText='';
+
+  changeText(){
+    this.randomText = lorem.sentences();
+    this.typedText='';
+  }
+  checkText(event:any){
+      this.typedText = event.target.value;
+  }
+
+  compare(enteredChar:string, randomText:string){
+    if(!enteredChar){
+      return 'pending';
+    }
+    return (enteredChar ===randomText)? 'correct' : 'incorrect';
+  }
 }
